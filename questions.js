58b54d97e5d73dad38458c0d745b4e25e8aeda7d
els4rchive <codewithella@gmail.com>
2023-02-12 20:04:53 -0300

    commit

---
+++ questions.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        question: "3 + 4",
+        answers: [
+            { option: "2", correct: false },
+            { option: "7", correct: true },
+            { option: "8", correct: false },
+        ],
+    },
+    {
+        question: "9 * 9",
+        answers: [
+            { option: "81", correct: true },
+            { option: "82", correct: false },
+            { option: "18", correct: false },
+        ], 
+    },
+    {
+        question: "8 * 0 + 4",
+        answers: [
+            { option: "0", correct: false },
+            { option: "4", correct: true },
+            { option: "32", correct: false },
+        ], 
+    },
+    {
+        question: " 9 * 9 - 9 + 9",
+        answers: [
+            { option: "81", correct: true },
+            { option: "0", correct: false },
+            { option: "39", correct: false },
+        ],
+    },
+
+];
